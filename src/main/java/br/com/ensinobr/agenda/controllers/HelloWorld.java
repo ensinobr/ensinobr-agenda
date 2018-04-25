9c4cddecc748677e4732731c385db44352be6a5b
ensinobr <ensinobr@localhost>
2018-04-25 18:25:38 -0300

    1 - Criando o projeto

---
+++ src/main/java/br/com/ensinobr/agenda/controllers/HelloWorld.java
@@ -0,0 +1,15 @@
+
+package br.com.ensinobr.agenda.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        
+        return "helloWorld";
+    }
+}
